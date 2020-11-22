dee19fbe4547af555285f556d3203d6c4dd5bc31
LIA <lvxing29@naver.com>
2020-11-22 16:07:43 +0900

    req res test+npm starter patch

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express")
 
 const app = express()
 
+//req res test
+app.use((req, res) =>{
+    res.json({
+        message: "서버 시작"
+    })
+})
+
 const port = 4000
 
 app.listen(port, console.log("server started"))
