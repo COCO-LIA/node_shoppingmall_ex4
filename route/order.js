542c0538b503c9b15f3fef9597970d9875f3beb3
LIA <lvxing29@naver.com>
2020-12-03 21:03:34 +0900

    order CRUD

--- route/order.js
+++ route/order.js
@@ -8,10 +8,65 @@ const orderModel = require('../model/order')
 
 //order 불러오는 API
 router.get("/", (req, res) => {
-    res.json({
-        msg: "order 불러오기 "
-    })
+    // res.json({
+    //     msg: "order 불러오기 "
+    // })
+
+    orderModel
+        .find()
+        .populate("chineselist", ["character","pingyin"])
+        .then(docs => {
+            res.json({
+                msg: "order total ",
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        id: doc._id,
+                        중국어:doc.chineselist,
+                        갯수:doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: "http://localhost:4000/ccorder/"+doc._id
+                        }
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+
 })
+
+//상세 불러오는 API
+router.get("/:orderId", (req, res) => {
+
+    orderModel
+        .findById(req.params.orderId)
+        .populate("chineselist", ["character","pingyin"])
+        .then(item => {
+            res.json({
+                msg: "order data" + item._id,
+                orderInfo: {
+                    id: item._id,
+                    중국어: item.chineselist,
+                    갯수: item.quantity,
+                    request:{
+                        type: 'GET',
+                        url: "http://localhost:4000/ccorder"
+                    }
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
+})
+
 
 
 //order 등록하는 API
@@ -40,7 +95,29 @@ router.post("/", (req, res) => {
 
 
 //order 수정하는 API
-router.patch("/", (req, res) => {
+router.patch("/:orderId", (req, res) => {
+
+    const updateOps = {}
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value
+    }
+
+    orderModel
+        .findByIdAndUpdate(req.params.orderId,{ $set: updateOps})
+        .then(() => {
+            res.json({
+                msg: "order 수정완료// " + req.params.orderId,
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:4000/ccorder/" + req.params.orderId
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
 
 })
 
@@ -48,6 +125,22 @@ router.patch("/", (req, res) => {
 //order 삭제하는 API
 router.delete("/", (req, res) => {
 
+    orderModel
+        .deleteMany()
+        .then(()=> {
+            res.json({
+                msg: "deleted ",
+                request: {
+                    type: 'GET',
+                    url: "http://localhost:4000/ccorder"
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                msg: err.message
+            })
+        })
 })
 
 
