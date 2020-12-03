f5a869d27771391f426e4032469064a5401ebd05
LIA <lvxing29@naver.com>
2020-12-03 17:14:48 +0900

    Add order post (register order)

--- route/order.js
+++ route/order.js
@@ -1,18 +1,40 @@
 const express = require("express")
 const router = express.Router()
 
+const orderModel = require('../model/order')
+
+
 //API 생성하기
 
 //order 불러오는 API
 router.get("/", (req, res) => {
     res.json({
-        msg: "order 불러오"
+        msg: "order 불러오기 "
     })
 })
 
 
 //order 등록하는 API
 router.post("/", (req, res) => {
+
+    const  orderInfo = new orderModel({
+        chineselist: req.body.chid,
+        quantity: req.body.qty
+    })
+
+    orderInfo
+        .save()
+        .then(item => {
+            res.json({
+                msg: " 중국어 목록 장바구니 ",
+                orderInfo: item
+            })
+        })
+        .catch(err=> {
+            res.json({
+                msg: err.message
+            })
+        })
 
 })
 
